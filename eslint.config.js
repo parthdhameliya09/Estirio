2d3b40b36a752236b722f78ee98b89972669d9f1
Parth Dhameliya <parth.d@7span.com>
2026-06-04 15:37:45 +0530

    initialised boilerplate code

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    ignores: ['node_modules/**', 'dist/**', 'build/**'],
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+    },
+  }
+);
