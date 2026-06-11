f42e19985b5e1dcbb11c98d0b94026fac7cfb48a
simaran-7span <simaran@7span.com>
2026-06-11 19:08:03 +0530

    remove obsolete migration files and clean up migration lock

--- prisma/migrations/20260609050154_fieldnames/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `updated_at` on the `bus_operators` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "bus_operators" DROP COLUMN "updated_at";
